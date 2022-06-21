d53a7c6ebee55a73a6c9610c014dfe1f77f64e48
Kenramiscal1106 <ramiscalken@gmail.com>
2022-06-21 22:41:45 +0800

    - added file

---
+++ src/components/svelte/Todo.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+  import TodoForm from "./TodoForm.svelte";
+  import TodoItems from "./TodoItems.svelte";
+  let todos = [];
+  onMount(() => {
+    const todosFromLS = localStorage.getItem("todos");
+
+    if (!todosFromLS) {
+      console.log("todos don't exist");
+      return;
+    }
+    todos = JSON.parse(todosFromLS);
+  });
+  $:localStorage.setItem('todos', JSON.stringify(todos))
+</script>
+
+<main>
+  <TodoForm bind:todos={todos}/>
+  <TodoItems {todos} />
+</main>
