edaa5fcb79d28f3ffac2312918abe3d434c9cf1a
Kenramiscal1106 <ramiscalken@gmail.com>
2022-06-21 22:41:58 +0800

    - added file for mounting in todo

---
+++ src/components/svelte/TodoForm.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let todos;
+  export let todoText;
+</script>
+
+<form on:submit|preventDefault={(e) => {
+  const formData = new FormData(e.target)
+  const formDataEntries = Object.fromEntries(formData.entries())
+  const todoToAdd = {
+    ...formDataEntries,
+    id: Math.random()
+  }
+  todos = [...todos, todoToAdd]
+  todoText = ""
+}}>
+  <label for="todo-text" >todo</label>
+  <input type="text" id="todo-text" name="text" bind:value={todoText}>
+  <button type="submit">Add todo</button>
+</form>
+
+<style></style>
