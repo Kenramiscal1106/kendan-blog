7e6d63caff2f64aba76f41d9afe409a2115fc020
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-09-03 18:56:07 +0800

    fix: change order svelte and tw config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,12 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    svelte(),
     tailwind({
       config: {
         applyBaseStyles: false,
       },
     }),
+    svelte(),
   ],
   server: {
     port: 8080,
