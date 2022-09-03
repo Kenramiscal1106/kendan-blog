7717f8f9e43741543224df61938142d3d842bcd9
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-09-03 18:58:44 +0800

    build: :lipstick: :sparkles: add design system

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "light-primary": "hsl(0,0%,100%)",
+        "light-secondary": "hsl(220, 35%,100%)",
+        "light-content": "hsl(0,0%,9%)",
+        "light-content-secondary": "hsl(0,0%,30%)",
+        "dark-primary": "hsl(0, 0%, 13%)",
+        "dark-secondary": "hsl(220, 12%, 20%)",
+        "dark-content": "hsl(0, 0%, 100%)",
+        "dark-content-secondary": "hsl(220, 12%, 70%)",
+      },
+    },
+  },
+  plugins: [],
+};
