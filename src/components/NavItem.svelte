54a237140d2bf6e92bb04703c8d49dae0ae8e259
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-09-04 03:17:35 +0800

    feat: :sparles: add props, styling

--- src/components/NavItem.svelte
+++ src/components/NavItem.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-  export let isActive: boolean;
+  export let isActive: boolean = false;
+  export let type: "text" | "icon-only" = "text";
 </script>
 
-<button class={`px-3 py-2 rounded-lg`}><slot /></button>
+<button
+  class={`${
+    type === "text" ? "px-3 py-2" : "p-2"
+  } rounded-lg text-light-content fill-current dark:text-dark-content ${
+    isActive
+      ? "bg-[#dde3ee] dark:bg-[#343842] font-semibold"
+      : "hover:bg-light-secondary dark:hover:bg-dark-secondary active:bg-[#dde3ee] active:dark:bg-[#343842]"
+  }`}><slot /></button
+>
