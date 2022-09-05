724d19829d70c24dde36334ed8dc420d50a1f9aa
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-09-06 02:06:40 +0800

    style: add bg-color and transition states

--- src/components/ThemeToggle.svelte
+++ src/components/ThemeToggle.svelte
@@ -20,7 +20,7 @@
 </script>
 
 <button
-  class="text-light-content dark:text-dark-content fill-none stroke-current"
+  class="text-light-content dark:text-dark-content fill-none stroke-current bg-light-secondary dark:bg-dark-secondary active:ring-light-content-secondary dark:active:ring-dark-content-secondary p-2 active:ring-2 rounded-lg transition-all duration-100"
   on:click={() => (isDark = !isDark)}
 >
   {#if isDark}
@@ -32,6 +32,7 @@
     >
       <path
         d="M12 3V5.25M18.364 5.63604L16.773 7.22703M21 12H18.75M18.364 18.364L16.773 16.773M12 18.75V21M7.22703 16.773L5.63604 18.364M5.25 12H3M7.22703 7.22703L5.63604 5.63604M15.75 12C15.75 14.0711 14.0711 15.75 12 15.75C9.92893 15.75 8.25 14.0711 8.25 12C8.25 9.92893 9.92893 8.25 12 8.25C14.0711 8.25 15.75 9.92893 15.75 12Z"
+        fill="none"
         stroke-width="1.5"
         stroke-linecap="round"
         stroke-linejoin="round"
