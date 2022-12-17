8f18644d1b03d36db5beafc00cc0860cd2546097
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-12-18 02:25:05 +0800

    add image optimization integration to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,26 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    svelte(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), svelte(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
   server: {
     port: 5050,
     host: "127.0.0.1"
   },
   markdown: {
     shikiConfig: {
-      theme: "one-dark-pro",
-    },
-  },
+      theme: "one-dark-pro"
+    }
+  }
 });
